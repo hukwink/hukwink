18404f322294b0a7446c0ed9f572f0948cc9292d
Karlatemp <kar@kasukusakura.com>
2023-12-26 19:34:37 +0800

    [util] Move ByteArrayHexStringSerializer to module hukwink-util

---
+++ hukwink-util/src/ByteArrayHexStringSerializer.kt
@@ -0,0 +1,20 @@
+package com.hukwink.hukwink.util
+
+import io.netty.buffer.ByteBufUtil
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+public object ByteArrayHexStringSerializer : KSerializer<ByteArray> {
+    override val descriptor: SerialDescriptor = String.serializer().descriptor
+
+    override fun deserialize(decoder: Decoder): ByteArray {
+        return ByteBufUtil.decodeHexDump(String.serializer().deserialize(decoder))
+    }
+
+    override fun serialize(encoder: Encoder, value: ByteArray) {
+        String.serializer().serialize(encoder, ByteBufUtil.hexDump(value))
+    }
+}
